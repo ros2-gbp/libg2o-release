e6000684651e40eaa6f3f6700184836c961d68f2
Steven! Ragnarök <steven@nuclearsandwich.com>
2020-06-16 08:58:31 -0400

    Rebase from 'debian/rolling/libg2o'

--- unit_test/sim3/allocate_sim3.cpp
+++ unit_test/sim3/allocate_sim3.cpp
@@ -1,5 +1,5 @@
 // g2o - General Graph Optimization
-// Copyright (C) 2011 H. Strasdat
+// Copyright (C) 2011 R. Kuemmerle, G. Grisetti, W. Burgard
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
@@ -24,25 +24,14 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#ifndef G2O_SE3_OPS_H
-#define G2O_SE3_OPS_H
+#include "g2o/types/sim3/types_seven_dof_expmap.h"
 
-#include <Eigen/Core>
-#include <Eigen/Geometry>
+#include "gtest/gtest.h"
 
-#include "g2o_types_slam3d_api.h"
-
-namespace g2o {
-
-  inline G2O_TYPES_SLAM3D_API Matrix3 skew(const Vector3&v);
-  inline G2O_TYPES_SLAM3D_API Vector3 deltaR(const Matrix3& R);
-  inline G2O_TYPES_SLAM3D_API Vector2 project(const Vector3&);
-  inline G2O_TYPES_SLAM3D_API Vector3 project(const Vector4&);
-  inline G2O_TYPES_SLAM3D_API Vector3 unproject(const Vector2&);
-  inline G2O_TYPES_SLAM3D_API Vector4 unproject(const Vector3&);
-
-  #include "se3_ops.hpp"
-
+TEST(Sim3, VertexSE3ExpmapCtor)
+{
+  auto* v = new g2o::VertexSE3Expmap();
+  ASSERT_NE(nullptr, v);
+  delete v;
+  SUCCEED();
 }
-
-#endif //MATH_STUFF
