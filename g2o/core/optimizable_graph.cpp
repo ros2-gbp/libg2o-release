d0c4b5e56bd50164e82ddc9daf5b599ab26c47bc
Scott K Logan <logans@cottsay.net>
2025-04-22 19:47:58 -0500

    Rebase from 'debian/kilted/libg2o'

--- g2o/core/optimizable_graph.cpp
+++ g2o/core/optimizable_graph.cpp
@@ -135,14 +135,16 @@ bool OptimizableGraph::Edge::resolveParameters() {
   for (size_t i = 0; i < _parameters.size(); i++) {
     int index = _parameterIds[i];
     *_parameters[i] = graph()->parameter(index);
+    if (!*_parameters[i]) {
+      G2O_CRITICAL(
+          "parameter {} is NULL. Check setParameterId() calls for the edge.",
+          i);
+      return false;
+    }
     auto& aux = **_parameters[i];
     if (typeid(aux).name() != _parameterTypes[i]) {
       G2O_CRITICAL("parameter type mismatch - encountered {}; should be {}",
                    typeid(aux).name(), _parameterTypes[i]);
-    }
-    if (!*_parameters[i]) {
-      G2O_CRITICAL("*_parameters[i] == 0");
-      return false;
     }
   }
   return true;
@@ -228,12 +230,12 @@ bool OptimizableGraph::addEdge(OptimizableGraph::Edge* e) {
   e->_internalId = _nextEdgeId++;
   if (e->numUndefinedVertices()) return true;
   if (!e->resolveParameters()) {
-    G2O_ERROR("{}: FATAL, cannot resolve parameters for edge {}",
+    G2O_ERROR("FATAL, cannot resolve parameters for edge {}",
               static_cast<void*>(e));
     return false;
   }
   if (!e->resolveCaches()) {
-    G2O_ERROR("{}: FATAL, cannot resolve caches for edge {}",
+    G2O_ERROR("FATAL, cannot resolve caches for edge {}",
               static_cast<void*>(e));
     return false;
   }
@@ -263,12 +265,12 @@ bool OptimizableGraph::setEdgeVertex(HyperGraph::Edge* e, int pos,
     OptimizableGraph::Edge* ee = static_cast<OptimizableGraph::Edge*>(e);
 #endif
     if (!ee->resolveParameters()) {
-      G2O_ERROR("{}: FATAL, cannot resolve parameters for edge {}",
+      G2O_ERROR("FATAL, cannot resolve parameters for edge {}",
                 static_cast<void*>(e));
       return false;
     }
     if (!ee->resolveCaches()) {
-      G2O_ERROR("{}: FATAL, cannot resolve caches for edge {}",
+      G2O_ERROR("FATAL, cannot resolve caches for edge {}",
                 static_cast<void*>(e));
       return false;
     }
@@ -473,14 +475,14 @@ bool OptimizableGraph::load(istream& is) {
       }
       if (!vertsOkay) {
         G2O_ERROR("Unable to find vertices for edge {} at line {} IDs: {}",
-                  token, lineNumber, fmt::join(ids, " "));
+                  token, lineNumber, strJoin(ids.begin(), ids.end(), " "));
         delete e;
         e = nullptr;
       } else {
         bool r = e->read(currentLine);
         if (!r || !addEdge(e)) {
           G2O_ERROR("Unable to add edge {} at line {} IDs: {}", token,
-                    lineNumber, fmt::join(ids, " "));
+                    lineNumber, strJoin(ids.begin(), ids.end(), " "));
           delete e;
           e = nullptr;
         }
@@ -808,10 +810,10 @@ bool OptimizableGraph::verifyInformationMatrices(bool verbose) const {
           ids[i] = e->vertex(i)->id();
         if (!isSymmetric)
           G2O_WARN("Information Matrix for an edge is not symmetric: {}",
-                   fmt::join(ids, " "));
+                   strJoin(ids.begin(), ids.end(), " "));
         else
           G2O_WARN("Information Matrix for an edge is not SPD: {}",
-                   fmt::join(ids, " "));
+                   strJoin(ids.begin(), ids.end(), " "));
         if (isSymmetric)
           G2O_WARN("eigenvalues: {}", eigenSolver.eigenvalues().transpose());
       }
